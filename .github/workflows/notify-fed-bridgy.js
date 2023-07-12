b2fb61e8a9e0b022eb839a395e2471d5cf285342
tyage <namatyage@gmail.com>
2023-07-12 15:53:55 +0900

    Remove quote

--- .github/workflows/notify-fed-bridgy.js
+++ .github/workflows/notify-fed-bridgy.js
@@ -3,10 +3,10 @@ const child_process = require('child_process')
 // webmention送りすぎないよう、最大値を設定
 const MAX_MENTION_SENDS = 10
 
-const diff = child_process.spawnSync('git', ['diff', 'HEAD^']).toString()
-const links = diff.matchAll(/<link>([^<]+)<\/link>/g)
+const diff = child_process.spawnSync('git', ['diff', 'HEAD^']).stdout.toString()
+const links = [...diff.matchAll(/<link>([^<]+)<\/link>/g)]
 for (let i = 0; i < links.length && i < MAX_MENTION_SENDS; ++i) {
-  const link = links[i]
+  const link = links[i][1]
   console.log(`Sending a webmention from ${link}...`)
-  child_process.spawnSync('curl', ['https://fed.brid.gy/webmention', '-d', `source="${encodeURIComponent(link)}"`, '-d', `target="https://fed.brid.gy"`])
+  child_process.spawnSync('curl', ['https://fed.brid.gy/webmention', '-d', `source=${encodeURIComponent(link)}`, '-d', `target=https://fed.brid.gy`])
 }
